7b2554e989d83dcdee53c0002354464f694baf22
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-07-12 00:32:34 -0400

    ZooProject-Validation

--- validation.js
+++ validation.js
@@ -1,32 +1,31 @@
 function validation(){
-    var fname= document.getElementById("t1").value;
-    var lname= document.getElementById("t2").value;
-    var age= document.getElementById("t3").value;
+    var email= document.getElementById("t1").value;
+    var fname= document.getElementById("t2").value;
+    var lname= document.getElementById("t3").value;
     var isValid = true;
 
-    if(fname==""){
-        document.getElementById("t1result").innerHTML="First name is required";
+    if(email==""){
+        document.getElementById("email1").innerHTML="Email name is required";
         isValid=false;
     }
     else
-        document.getElementById("t1result").innerHTML="";
+        document.getElementById("email1").innerHTML="";
+
+    if(fname==""){
+        document.getElementById("fname1").innerHTML="First name is required";
+        isValid=false;
+    }
+    else
+        document.getElementById("fname1").innerHTML="";
 
     if(lname==""){
-        document.getElementById("t2result").innerHTML="Last name is required";
+        document.getElementById("lname1").innerHTML="Last name is required";
         isValid=false;
     }
     else
-        document.getElementById("t2result").innerHTML="";
+        document.getElementById("lname1").innerHTML="";
 
-    if(age==""){
-        document.getElementById("t3result").innerHTML="Age is required";
-        isValid=false;
-    }
-    else
-        document.getElementById("t3result").innerHTML="";
-
-    //form needs to be submitted
     if(isValid == true)
         document.getElementById("f1").submit();
     
-}//end of function
+}
